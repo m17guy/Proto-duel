119f4163af450f085f2a69b46665e827b38c95b3
m17guy <m1734guy@gmail.com>
2018-08-23 21:17:32 +0300

    just geting started

---
+++ making the deck/making the deck/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace making_the_deck
+{
+    class Card
+    {
+        public int randomnumber;
+        public int points;
+        public int age;
+        public int[,] coust;
+        public string willCunstract;
+        public string freeCunstraction;
+        public string name;
+    }
+}
---
+++ making the deck/making the deck/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace making_the_deck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+        static Card[] ShafelTamar(Card[] a)
+        {
+            Random num = new Random();
+            for (int z = 0; z < a.Length; z++)
+            {
+                a[z].randomnumber = num.Next(1, 1000);
+            }
+            Array.Sort(a, delegate (Card a1, Card b2) { return a1.randomnumber.CompareTo(b2.randomnumber); });
+            return a;
+        }
+    }
+}
